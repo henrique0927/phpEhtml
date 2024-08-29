5ec9a888bc6f2937b197b689c779e0cdca556c2f
henrique0927 <henrique_stolle@estudante.sesisenai.org.br>
2024-08-29 12:02:07 -0300

    css arrumando

--- aula2/index.php
+++ aula2/index.php
@@ -15,29 +15,28 @@
             <input autocomplete="off" name="base" id="Email" class="input" type="number" required>
             <div id="button">
                 <button type="submit" name="verificar_envio">Verificar</button>
-            </div>
+            </div> 
+            <?php
+            //verificar o método enviado
+                if($_SERVER["REQUEST_METHOD"] == "POST"){
+                    // Verificar se não foi enviado algo vazio, (isset)
+                    if(isset($_POST["verificar_envio"])){
+                        // calcular a área do triângulo -->> bh/2
+                        $altura = $_POST["altura"];
+                        $base = $_POST["base"];
+                        $area = ($base * $altura) / 2;
+                        // Verifique se a área calculada é maior do que um valor limite 100.
+                        if($area > 100){
+                            // Exiba uma mensagem informando se a área é maior ou menor/igual ao valor limite.
+                            echo "<p>Área maior que o limite de 100.</p>";
+                        }else{
+                            // Exiba uma mensagem informando se a área é maior ou menor/igual ao valor limite.
+                            echo "<p>Área: ", $area,"</p>";
+                        }
+                    }
+                }
+            ?>
         </div>
     </form>
-
-    <?php
-    //verificar o método enviado
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            // Verificar se não foi enviado algo vazio, (isset)
-            if(isset($_POST["verificar_envio"])){
-                // calcular a área do triângulo -->> bh/2
-                $altura = $_POST["altura"];
-                $base = $_POST["base"];
-                $area = ($base * $altura) / 2;
-                // Verifique se a área calculada é maior do que um valor limite 100.
-                if($area > 100){
-                    // Exiba uma mensagem informando se a área é maior ou menor/igual ao valor limite.
-                    echo "<p>Área maior que o limite de 100.</p>";
-                }else{
-                    // Exiba uma mensagem informando se a área é maior ou menor/igual ao valor limite.
-                    echo "<p>Área: ", $area,"</p>";
-                }
-            }
-        }
-    ?>
 </body>
 </html>
